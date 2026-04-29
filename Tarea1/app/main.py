b58559f77e3bec7c5bb0155fdf34d23af5cf3a9e
Mair <163215578+MaiR42@users.noreply.github.com>
2026-04-28 23:22:18 -0400

    Add main function to main.py with greeting

---
+++ Tarea1/app/main.py
@@ -0,0 +1,11 @@
+from queries import q1_count, q2_area, q3_density, q4_compare, q5_confidence_dist
+from traffic_gen import generate_uniform_queries, generate_zipf_queries
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+def main():
+    print("hola world")
+
+if __name__ == "__main__":
+    main()

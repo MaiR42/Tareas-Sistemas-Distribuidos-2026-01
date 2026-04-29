f883e66bd72e74b053bbb98c66797afc654b9046
Mair <163215578+MaiR42@users.noreply.github.com>
2026-04-29 12:50:29 -0400

    Modify traffic generation functions and add timing
    Updated the number of queries in the Zipf and uniform traffic generation functions to 100 and added timing functionality.

--- Tarea1/app/traffic_gen.py
+++ Tarea1/app/traffic_gen.py
@@ -1,13 +1,20 @@
 import numpy as np
 import random
+import time
 
 # Trafico Zipf 
 # => Unas zonas mas frecuentes que otras
 zones = ["Z1","Z2","Z3","Z4","Z5"]
 
-# Default (para probar) n=20 "consultas" y a=1.5 "sesgo"
+# Default (para probar) n=100 "consultas" y a=1.5 "sesgo"
 # (entre mas alto mayor sea "a" la diferencia de las prob de zona (z1 -> 70%, z2 -> 20%, z3 -> 8%...))
-def generate_zipf_queries(n=20, a=1.5):
+
+init_time = 0
+final_time = 0
+
+def generate_zipf_queries(n=100, a=1.5):
+    global init_time, final_time
+    init_time = time.perf_counter()
     queries = []
     for _ in range(n):
         rank = np.random.zipf(a)
@@ -15,9 +22,18 @@ def generate_zipf_queries(n=20, a=1.5):
         rank = ((rank-1) % 5)
 
         queries.append(zones[rank])
+    final_time = time.perf_counter() - init_time
     return queries
 
 
 # Trafico uniforme
-def generate_uniform_queries(n=20):
-    return random.choices(zones,k=n)
+def generate_uniform_queries(n=100):
+    global init_time, final_time
+    init_time = time.perf_counter()
+    queries = random.choices(zones,k=n)
+    final_time = time.perf_counter() - init_time
+    return queries
+
+def gen_time():
+    global final_time
+    print("Tiempo en generar datos: ", final_time)

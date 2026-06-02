c0cbe6495be52c196a6b8afab04ae502f277b448
MaiR42 <mair.escobar@mail.udp.cl>
2026-06-01 23:32:58 -0400

    Archivo con configuraciones

---
+++ Tarea2/app/config.py
@@ -0,0 +1,10 @@
+# Para facilitar el uso de variables y parametros
+# Politica de remocion, espacio de memoria cache y TRAFFIC_INTERVAL sigue en el docker-compose.yml
+
+# T1
+TTL = 60 # engine.py
+GEN_TRAFIC_DISTR = "zipf" # "zipf" o "uniform" # main.py
+
+# T2
+MAX_RETRIES = 3 # retry_manager.py # Despues
+

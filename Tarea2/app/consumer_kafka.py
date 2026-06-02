6c9567c15b218693f4bb55cd67a056e872e72417
MaiR42 <mair.escobar@mail.udp.cl>
2026-06-02 00:14:44 -0400

    Prueba de Kafka

--- Tarea2/app/consumer_kafka.py
+++ Tarea2/app/consumer_kafka.py
@@ -1,1 +1,24 @@
 # Kafka consumer
+from kafka import KafkaConsumer
+import json
+
+# Config del consumer
+consumer = KafkaConsumer(
+    "consultas",
+
+    bootstrap_servers="kafka:9092",
+
+    value_deserializer=lambda m:
+        json.loads(m.decode("utf-8")),
+
+    auto_offset_reset="earliest",
+
+    group_id="grupo1"
+)
+
+# Testeo
+print("Esperando mensajes...")
+
+for msg in consumer:
+
+    print(msg.value)
--- Tarea2/app/producer_kafka.py
+++ Tarea2/app/producer_kafka.py
@@ -1,1 +1,24 @@
 # Kafka producer
+from kafka import KafkaProducer
+import json
+
+# Config del producer
+producer = KafkaProducer(
+    bootstrap_servers="kafka:9092",
+    value_serializer=lambda v: json.dumps(v).encode("utf-8")
+)
+
+
+# Testeo
+mensaje = {
+    "test": "hola kafka"
+}
+
+producer.send(
+    "consultas",
+    mensaje
+)
+
+producer.flush()
+
+print("Mensaje enviado")

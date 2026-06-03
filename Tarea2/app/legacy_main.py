9ccd9a1ee86df02f28e2e7532f90bdf87096c195
MaiR42 <mair.escobar@mail.udp.cl>
2026-06-02 23:50:30 -0400

    Archivo main (No utilizado)

---
+++ Tarea2/app/legacy_main.py
@@ -0,0 +1,108 @@
+# Ya no esta en uso, pq se ocupa consumer y producer ahora
+
+# Para generar trafico
+import os
+import json
+import time
+import redis
+
+from kafka import KafkaProducer
+producer = KafkaProducer(
+    bootstrap_servers="kafka:9092",
+    value_serializer=lambda v: json.dumps(v).encode("utf-8")
+)
+
+
+from config import GEN_TRAFIC_DISTR, TTL, MAX_RETRIES
+from config import TTL
+from traffic_gen import generate_query
+
+r = redis.Redis(
+    host=os.getenv("REDIS_HOST", "cache"),
+    port=6379,
+    decode_responses=True
+)
+
+distribution = os.getenv("TRAFFIC_DIST", GEN_TRAFIC_DISTR)
+interval = float(os.getenv("TRAFFIC_INTERVAL", "1"))
+
+# DEBUG TEXT
+print()
+print(f"Generador iniciado con distribución: {distribution}")
+print(f"Sistema cache iniciado con TTL: {TTL}")
+print(f"Sistema de manejo de fallos iniciado con maximo de reintentos: {MAX_RETRIES}")
+print()
+
+
+def build_payload(q):
+    tipo = q["tipo"]
+
+    # -------- PARAMS --------
+    if tipo in ["Q1", "Q2", "Q3"]:
+        params = {
+            "zone_id": q["zona"],
+            "confidence_min": q.get("confidence_min", 0.0)
+        }
+
+    elif tipo == "Q4":
+        params = {
+            "zone_a": q["zona"],
+            "zone_b": q["zona_b"],
+            "confidence_min": q.get("confidence_min", 0.0)
+        }
+
+    elif tipo == "Q5":
+        params = {
+            "zone_id": q["zona"],
+            "bins": q.get("bins", 5)
+        }
+
+    else:
+        params = {}
+
+    # -------- CACHE KEY --------
+    if tipo == "Q1":
+        key = f"count:{params['zone_id']}:conf={params.get('confidence_min', 0.0)}"
+
+    elif tipo == "Q2":
+        key = f"area:{params['zone_id']}:conf={params.get('confidence_min', 0.0)}"
+
+    elif tipo == "Q3":
+        key = f"density:{params['zone_id']}:conf={params.get('confidence_min', 0.0)}"
+
+    elif tipo == "Q4":
+        key = f"compare:density:{params['zone_a']}:{params['zone_b']}:conf={params.get('confidence_min', 0.0)}"
+
+    elif tipo == "Q5":
+        key = f"confidence_dist:{params['zone_id']}:bins={params.get('bins', 5)}"
+
+    else:
+        key = "unknown"
+
+    return {
+        "type": tipo,
+        "params": params,
+        "cache_key": key
+    }
+
+
+def main():
+    while True:
+        raw_query = generate_query(distribution=distribution)
+
+        payload = build_payload(raw_query)
+
+        #r.rpush("cola:consultas", json.dumps(payload)) ### D
+
+        producer.send(
+            "consultas",
+            payload
+        )
+
+        print(f"Enviada: Tipo: {payload['type']}, Cache_key: {payload['cache_key']}")
+
+        time.sleep(interval)
+
+
+if __name__ == "__main__":
+    main()
